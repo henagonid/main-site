120cb7a5f8a16b5861f03be48b57b4a15383ba99
henagon <hey@henagon.id>
2024-01-08 10:31:26 +0700

    add gtm

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly PUBLIC_GTM_ID: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
